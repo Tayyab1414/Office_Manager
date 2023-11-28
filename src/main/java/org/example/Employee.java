8cdc2a972fa98f525aad4f150aa362abd5120cca
Tayyab <tayyab.abbasi1414@gmail.com>
2023-11-28 15:13:22 +0100

    Initial commit

---
+++ src/main/java/org/example/Employee.java
@@ -0,0 +1,26 @@
+import org.example.Person;
+
+public class Employee extends Person {
+
+    public Employee(String name){
+        setName(name);
+    }
+    public String getSound(){
+        return "Aaow!";
+    }
+    public String getName(){
+        return name;
+    }
+    public void setName(String name){
+        this.name = capitalizeFirstLetter(name);
+    }
+   @Override
+   public String eatFood(){
+        return "Nom nom nom";
+   }
+   protected String capitalizeFirstLetter(String input){
+        String firstCharacter = input.substring(0,1).toUpperCase();
+        String rest = input.substring(1);
+        return  firstCharacter;
+   }
+}
